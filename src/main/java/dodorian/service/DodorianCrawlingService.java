52f181bf1ea9f92c20a7f511ca88ec3ecc5aaf14
jhcho <jhcho7510@gmail.com>
2020-07-23 00:49:50 +0900

    깃 테스트

--- src/main/java/dodorian/service/DodorianCrawlingService.java
+++ src/main/java/dodorian/service/DodorianCrawlingService.java
@@ -71,8 +71,6 @@ public class DodorianCrawlingService {
             logger.info("-----------------------------------------------------------------------------");
 		}
 		
-		
-		//logger.info(doc.toString());
 	}
 	
 }
